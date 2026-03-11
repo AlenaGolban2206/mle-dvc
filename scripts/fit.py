c3c50c6c64c0889c06514c7e2e5671b5cc5d1561
Alena Golban <alenkagolban@yandex.ru>
2026-03-11 10:25:35 +0000

    Restore CatBoost in base_model

--- scripts/fit.py
+++ scripts/fit.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
+from category_encoders import CatBoostEncoder
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.linear_model import LogisticRegression
+from catboost import CatBoostClassifier
 import yaml
 import os
 import joblib
@@ -12,28 +13,28 @@ def fit_model():
         params = yaml.safe_load(fd)
 
     data = pd.read_csv('data/initial_data.csv')
-    target_col = params.get('target_col', 'target')
-    X = data.drop(columns=[target_col])
-    y = data[target_col]
+    
+    cat_features = data.select_dtypes(include='object')
+    potential_binary_features = cat_features.nunique() == 2
 
-    cat_features = X.select_dtypes(include='object')
-    num_features = X.select_dtypes(['float'])
+    binary_cat_features = cat_features[potential_binary_features[potential_binary_features].index]
+    other_cat_features = cat_features[potential_binary_features[~potential_binary_features].index]
+    num_features = data.select_dtypes(['float'])
 
     preprocessor = ColumnTransformer([
-        ('cat', OneHotEncoder(drop=params.get('one_hot_drop', 'if_binary')), cat_features.columns.tolist()),
+        ('binary', OneHotEncoder(drop='if_binary'), binary_cat_features.columns.tolist()),
+        ('cat', CatBoostEncoder(return_df=False), other_cat_features.columns.tolist()),
         ('num', StandardScaler(), num_features.columns.tolist())
     ], remainder='drop', verbose_feature_names_out=False)
 
-    model = LogisticRegression(
-        penalty=params.get('penalty', 'l2'),
-        C=params.get('C', 1.0)
-    )
+    model = CatBoostClassifier(auto_class_weights='Balanced', verbose=False)
 
     pipeline = Pipeline([
         ('preprocessor', preprocessor),
         ('model', model)
     ])
-    pipeline.fit(X, y)
+    
+    pipeline.fit(data, data['target'])
 
     os.makedirs('models', exist_ok=True)
     with open('models/fitted_model.pkl', 'wb') as fd:
