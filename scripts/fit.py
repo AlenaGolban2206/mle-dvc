91565a551454a993ebc83320e5251a8e7c3ca96b
Alena Golban <alenkagolban@yandex.ru>
2026-03-11 17:26:23 +0000

    Переделала на Лог рег в1

--- scripts/fit.py
+++ scripts/fit.py
@@ -1,19 +1,24 @@
+# scripts/fit.py
+
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from category_encoders import CatBoostEncoder
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from catboost import CatBoostClassifier
+from sklearn.linear_model import LogisticRegression
 import yaml
 import os
 import joblib
 
+# обучение модели
 def fit_model():
+    # Прочитайте файл с гиперпараметрами params.yaml
     with open('params.yaml', 'r') as fd:
         params = yaml.safe_load(fd)
 
+    # загрузите результат предыдущего шага: inital_data.csv
     data = pd.read_csv('data/initial_data.csv')
-    
+
+    # реализуйте основную логику шага с использованием гиперпараметров
     cat_features = data.select_dtypes(include='object')
     potential_binary_features = cat_features.nunique() == 2
 
@@ -21,24 +26,31 @@ def fit_model():
     other_cat_features = cat_features[potential_binary_features[~potential_binary_features].index]
     num_features = data.select_dtypes(['float'])
 
-    preprocessor = ColumnTransformer([
-        ('binary', OneHotEncoder(drop='if_binary'), binary_cat_features.columns.tolist()),
-        ('cat', CatBoostEncoder(return_df=False), other_cat_features.columns.tolist()),
-        ('num', StandardScaler(), num_features.columns.tolist())
-    ], remainder='drop', verbose_feature_names_out=False)
+    preprocessor = ColumnTransformer(
+        [
+            ('binary', OneHotEncoder(drop=params['one_hot_drop']), binary_cat_features.columns.tolist()),
+            ('cat', OneHotEncoder(drop=params['one_hot_drop']), other_cat_features.columns.tolist()),
+            ('num', StandardScaler(), num_features.columns.tolist())
+        ],
+        remainder='drop',
+        verbose_feature_names_out=False
+    )
 
-    model = CatBoostClassifier(auto_class_weights='Balanced', verbose=False)
+    model = LogisticRegression(C=params['C'], penalty=params['penalty'])
 
-    pipeline = Pipeline([
-        ('preprocessor', preprocessor),
-        ('model', model)
-    ])
-    
-    pipeline.fit(data, data['target'])
+    pipeline = Pipeline(
+        [
+            ('preprocessor', preprocessor),
+            ('model', model)
+        ]
+    )
+    pipeline.fit(data, data[params['target_col']]) 
 
-    os.makedirs('models', exist_ok=True)
+    # сохраните обученную модель в models/fitted_model.pkl
+    os.makedirs('models', exist_ok=True) 
     with open('models/fitted_model.pkl', 'wb') as fd:
-        joblib.dump(pipeline, fd)
+        joblib.dump(pipeline, fd) 
+
 
 if __name__ == '__main__':
     fit_model()
